1494d94f8bc81ac0006f7cae0c0e5a276b900294
Karolina Lencina <karolencina@icloud.com>
2024-12-29 13:08:44 +0000

    Remove unused imports

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./index.module.css";
 import clx from "classnames";
 import Text from "../Text";
--- components/Text/index.stories.tsx
+++ components/Text/index.stories.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import Text from "@components/Text";
+
 const meta: Meta<typeof Text> = {
   component: Text,
 };
